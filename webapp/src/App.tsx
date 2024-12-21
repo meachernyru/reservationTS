4574afcf9453e19218ff29c21912a672b5286223
Aleksey Cherny <me.acherny.ru>
2024-12-21 16:49:09 +0300

    Added prettier

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,20 @@
 export const App = () => {
-  return <h1 className="my-class">Hello, world</h1>;
-};
+  const medservices = [
+    { id: '1', name: 'Service 1', price: 1.0 },
+    { id: '2', name: 'Service 2', price: 5.0 },
+    { id: '3', name: 'Service 3', price: 6.0 },
+    { id: '4', name: 'Service 4', price: 8.0 },
+  ]
+  return (
+    <div>
+      <h1 className="my-class">Медицинские услуги</h1>
+      {medservices.map((item) => {
+        return (
+          <h2 key={item.id}>
+            {item.name} цена: {item.price} руб.
+          </h2>
+        )
+      })}
+    </div>
+  )
+}
