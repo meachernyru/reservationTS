0f0017bd2dd638dc379574d55add79d401cd86d5
Aleksey Cherny <me.acherny.ru>
2024-12-21 18:43:00 +0300

    express installed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello  world')
+import express from 'express'
+
+const expressApp = express()
+
+expressApp.get('/ping', (reg, res) => {
+  res.send('Pong')
+})
+
+expressApp.listen(3000, () => {
+  console.info('Listening at http//localhost:3000')
+})
